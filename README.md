4ac3072ba32bf1b34189b0f458e1e89e4b029457
Ben Lauret <ben.lauret.wm@renesas.com>
2024-04-12 14:28:03 +0200

    First buildable

--- README.md
+++ README.md
@@ -1,54 +1,7 @@
 # Simulator project for LVGL embedded GUI Library
 
-The [LVGL](https://github.com/lvgl/lvgl) is written mainly for microcontrollers and embedded systems however you can run the library **on your PC** as well without any embedded hardware. The code written on PC can be simply copied when your are using an embedded system.
+build:
 
-## Requirements
-This project is configured for [VSCode](https://code.visualstudio.com) and only tested on Linux, although this may work on OSx or WSL. It requires a working version of GCC, GDB and make in your path.
-
-To allow debugging inside VSCode you will also require a GDB [extension](https://marketplace.visualstudio.com/items?itemName=webfreak.debug) or other suitable debugger. All the requirements, build and debug settings have been pre-configured in the [.workspace](simulator.code-workspace) file (simply open the project by doubleclick on this file).
-
-The project can use **SDL** but it can be easily relaced by any other built-in LVGL dirvers.
-
-## Usage
-
-### Get the PC project
-
-Clone the PC project and the related sub modules:
-
-```bash
-git clone --recursive https://github.com/lvgl/lv_port_pc_vscode
-```
-
-### Install graphics driver
-
-Please make sure **SDL**  is installed in the system:
-
-#### Install SDL
-You can download SDL from https://www.libsdl.org/
-
-On on Linux you can install it via terminal:
-```bash
-sudo apt-get update && sudo apt-get install -y build-essential libsdl2-dev
-```
-
-### Optional library
-There are also FreeType and FFmpeg support. You can install FreeType support with:
-```bash
-# FreeType support
-wget https://kumisystems.dl.sourceforge.net/project/freetype/freetype2/2.13.2/freetype-2.13.2.tar.xz
-tar -xf freetype-2.13.2.tar.xz
-cd freetype-2.13.2
-make
-make install
-```
-
-The FFmpeg support can be installed with:
-```bash
-# FFmpeg support
-git clone https://git.ffmpeg.org/ffmpeg.git ffmpeg
-cd ffmpeg
-git checkout release/6.0
-./configure --disable-all --disable-autodetect --disable-podpages --disable-asm --enable-avcodec --enable-avformat --enable-decoders --enable-encoders --enable-demuxers --enable-parsers --enable-protocol='file' --enable-swscale --enable-zlib
-make
-sudo make install
-```
+> cmake -B build -S . -G Ninja
+> ninja -C build
+> ./bin/main
--- main/src/main.c
+++ main/src/main.c
@@ -9,6 +9,7 @@
  *********************/
 #define _DEFAULT_SOURCE /* needed for usleep() */
 #include <stdlib.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <pthread.h>
 #include "lvgl/lvgl.h"
@@ -19,20 +20,26 @@
  *      DEFINES
  *********************/
 
-/**********************
- *      TYPEDEFS
- **********************/
+#if DEBUG
+#define ECU_LOADING_DONE 1
+#else
+#define ECU_LOADING_DONE 5
+#endif
 
-/**********************
- *  STATIC PROTOTYPES
- **********************/
-static lv_display_t * hal_init(int32_t w, int32_t h);
+    /**********************
+     *      TYPEDEFS
+     **********************/
+    /**********************
+     *  STATIC PROTOTYPES
+     **********************/
+    static lv_display_t *
+    hal_init(int32_t w, int32_t h);
 
 /**********************
  *  STATIC VARIABLES
  **********************/
 
-/********************** 
+/**********************
  *      MACROS
  **********************/
 
@@ -47,14 +54,83 @@ static lv_display_t * hal_init(int32_t w, int32_t h);
 /**********************
  *      TYPEDEFS
  **********************/
+typedef enum
+{
+  ECU_UI_LOADING,
+  ECU_UI_MAIN
 
+} ecu_ui_state_t;
+ecu_ui_state_t state = ECU_UI_LOADING;
 /**********************
  *      VARIABLES
  **********************/
+extern lv_img_dsc_t wrover_startup;
 
 /**********************
  *  STATIC PROTOTYPES
  **********************/
+static void auto_step_cb(lv_timer_t *timer);
+
+/**********************
+ *   Test code
+ **********************/
+
+void set_main_screen()
+{
+
+  lv_obj_t *count_label;
+
+  lv_obj_clean(lv_scr_act());
+
+  count_label = lv_label_create(lv_scr_act());
+  lv_label_set_text(count_label, "Welcome!!");
+  lv_obj_align(count_label, LV_ALIGN_BOTTOM_MID, 0, 0);
+}
+
+void set_startup_screen()
+{
+
+  lv_obj_t *hello_world_label, *img;
+  lv_obj_t *count_label;
+
+  img = lv_img_create(lv_scr_act());
+  lv_img_set_src(img, &wrover_startup);
+
+  // lv_label_set_text(hello_world_label, "Hello world!");
+  // lv_obj_align(hello_world_label, LV_ALIGN_CENTER, 0, 0);
+
+  count_label = lv_label_create(lv_scr_act());
+  lv_label_set_text(count_label, "Loading");
+  lv_obj_align(count_label, LV_ALIGN_BOTTOM_MID, 0, 0);
+
+  lv_timer_create(auto_step_cb, 1000, NULL);
+  // lv_task_handler();
+  // display_blanking_off(display_dev);
+}
+
+static void auto_step_cb(lv_timer_t *t)
+{
+  LV_UNUSED(t);
+  static uint32_t loading_delay = 0;
+
+  // const lv_font_t * font_small = &lv_font_montserrat_12;
+  // const lv_font_t * font_large = &lv_font_montserrat_16;
+  printf("delay %d", loading_delay);
+  switch (state)
+  {
+  case ECU_UI_LOADING:
+    loading_delay++;
+    if (ECU_LOADING_DONE == loading_delay)
+    {
+      state = ECU_UI_MAIN;
+      set_main_screen();
+    }
+    break;
+
+  case ECU_UI_MAIN:
+    break;
+  }
+}
 
 /**********************
  *   GLOBAL FUNCTIONS
@@ -69,11 +145,12 @@ int main(int argc, char **argv)
   lv_init();
 
   /*Initialize the HAL (display, input devices, tick) for LVGL*/
-  hal_init(480, 272);
+  hal_init(320, 240);
 
-  lv_demo_widgets();
+  set_startup_screen();
 
-  while(1) {
+  while (1)
+  {
     /* Periodically call the lv_task handler.
      * It could be done in a timer interrupt or an OS task too.*/
     lv_timer_handler();
@@ -91,29 +168,29 @@ int main(int argc, char **argv)
  * Initialize the Hardware Abstraction Layer (HAL) for the LVGL graphics
  * library
  */
-static lv_display_t * hal_init(int32_t w, int32_t h)
+static lv_display_t *hal_init(int32_t w, int32_t h)
 {
 
   lv_group_set_default(lv_group_create());
 
-  lv_display_t * disp = lv_sdl_window_create(w, h);
+  lv_display_t *disp = lv_sdl_window_create(w, h);
 
-  lv_indev_t * mouse = lv_sdl_mouse_create();
+  lv_indev_t *mouse = lv_sdl_mouse_create();
   lv_indev_set_group(mouse, lv_group_get_default());
   lv_indev_set_display(mouse, disp);
   lv_display_set_default(disp);
 
   LV_IMAGE_DECLARE(mouse_cursor_icon); /*Declare the image file.*/
-  lv_obj_t * cursor_obj;
+  lv_obj_t *cursor_obj;
   cursor_obj = lv_image_create(lv_screen_active()); /*Create an image object for the cursor */
-  lv_image_set_src(cursor_obj, &mouse_cursor_icon);           /*Set the image source*/
-  lv_indev_set_cursor(mouse, cursor_obj);             /*Connect the image  object to the driver*/
+  lv_image_set_src(cursor_obj, &mouse_cursor_icon); /*Set the image source*/
+  lv_indev_set_cursor(mouse, cursor_obj);           /*Connect the image  object to the driver*/
 
-  lv_indev_t * mousewheel = lv_sdl_mousewheel_create();
+  lv_indev_t *mousewheel = lv_sdl_mousewheel_create();
   lv_indev_set_display(mousewheel, disp);
   lv_indev_set_group(mousewheel, lv_group_get_default());
 
-  lv_indev_t * kb = lv_sdl_keyboard_create();
+  lv_indev_t *kb = lv_sdl_keyboard_create();
   lv_indev_set_display(kb, disp);
   lv_indev_set_group(kb, lv_group_get_default());
 
